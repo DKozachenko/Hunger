7c6d82b24935da520b0a504c415a10bfedee411c
Diana <kozachenkodiana03@mail.ru>
2020-08-20 22:54:13 +0700

    Booking

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const burger = document.querySelector('.burger'),
-  navs = document.querySelectorAll('.nav__left, .nav__right');
+  navs = document.querySelectorAll('.nav__left, .nav__right'),
+  
+  homeInnerDownbutton = document.querySelector('.home__inner-downbutton'),
+  home = document.querySelector('.home');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
@@ -9,3 +12,12 @@ burger.addEventListener('click', () => {
     item.classList.toggle('nav_show');
   })
 }) 
+
+homeInnerDownbutton.addEventListener('click', () => {
+  const homeHeight = home.offsetHeight;
+  
+  window.scrollTo({
+    top: homeHeight,
+    behavior: 'smooth'
+  })
+})
