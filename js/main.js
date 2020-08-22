9d638bde772636ccbb004f4a9d4fe7026f8037f4
Diana <kozachenkodiana03@mail.ru>
2020-08-22 20:37:14 +0700

    Menu, Events, Gallery

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ const burger = document.querySelector('.burger'),
   navs = document.querySelectorAll('.nav__left, .nav__right'),
   
   homeInnerDownbutton = document.querySelector('.home__inner-downbutton'),
-  home = document.querySelector('.home');
+  home = document.querySelector('.home'),
+  
+  contentNavLinks = document.querySelectorAll('.content__nav-link'),
+  columnItems = document.querySelectorAll('.column__item');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
@@ -26,4 +29,20 @@ $('.special__inner-slider').slick({
   infinite: true,
   arrows: false,
   dots: true
+});
+
+contentNavLinks.forEach(elem => {
+  elem.addEventListener('click', (event) => {
+    event.preventDefault();
+    const attrNav = elem.dataset.menuNav;
+    for (elem of columnItems) {
+      const attrRes = elem.dataset.menuResult;
+      if (attrRes === attrNav) {
+        elem.style.display = 'block';
+      } else {
+        elem.style.display = 'none';
+      }
+      //console.log(elem);
+    }
+  })
 });
