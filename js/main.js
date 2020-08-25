176ed73cd6e61efc8312dd17ac73d192725ccdca
Diana <kozachenkodiana03@mail.ru>
2020-08-25 11:14:19 +0700

    JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const burger = document.querySelector('.burger'),
   navs = document.querySelectorAll('.nav__left, .nav__right'),
   
@@ -7,15 +6,19 @@ const burger = document.querySelector('.burger'),
   home = document.querySelector('.home'),
   
   contentNavLinks = document.querySelectorAll('.content__nav-link'),
-  columnItems = document.querySelectorAll('.column__item');
-
+  columnItems = document.querySelectorAll('.column__item'), 
+  
+  header = document.querySelector('.header'),
+  
+  navLinks = document.querySelectorAll('.nav__left-link, .nav__right-link');
+//Burger
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
   navs.forEach(item => {
     item.classList.toggle('nav_show');
   })
 }) 
-
+//Scroll from homepage
 homeInnerDownbutton.addEventListener('click', () => {
   const homeHeight = home.offsetHeight;
   
@@ -24,25 +27,58 @@ homeInnerDownbutton.addEventListener('click', () => {
     behavior: 'smooth'
   })
 })
-
-$('.special__inner-slider').slick({
-  infinite: true,
-  arrows: false,
-  dots: true
-});
-
+//Menu tabs
 contentNavLinks.forEach(elem => {
   elem.addEventListener('click', (event) => {
     event.preventDefault();
     const attrNav = elem.dataset.menuNav;
-    for (elem of columnItems) {
-      const attrRes = elem.dataset.menuResult;
+    for (let item of columnItems) {
+      const attrRes = item.dataset.menuResult;
       if (attrRes === attrNav) {
-        elem.style.display = 'block';
+        item.style.display = 'block';
+        item.classList.add('column__item_active');
       } else {
-        elem.style.display = 'none';
+        item.style.display = 'none';
+        item.classList.remove('column__item_active');
       }
-      //console.log(elem);
+      
     }
   })
+});
+//Fixed header
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset;
+  let needHeight = home.offsetHeight;
+
+  if (scrollTop >= needHeight) {
+    header.classList.add('header_fixed');
+  } else {
+    header.classList.remove('header_fixed');
+  }
+})
+//Smooth scroll
+navLinks.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const blockName = item.dataset.link;
+    const block = document.querySelector(`.${blockName}`);
+    const scrollTop = block.offsetTop;
+
+    window.scrollTo({
+      top: scrollTop - 100,
+      behavior: "smooth"
+    })
+
+    burger.classList.remove('burger_active');
+    navs.forEach(item => {
+      item.classList.remove('nav_show');
+    })
+  })
+})
+//Slick slider
+$('.special__inner-slider').slick({
+  infinite: true,
+  arrows: false,
+  dots: true
 });
